5aa60ec00ae9ec52934dec15d1355345f561f9e6
murraylmichele-svg <murraylmichele@gmail.com>
2026-08-17 08:08:03 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // SW.JS — Service Worker for offline support
 // ============================================================
 
-var CACHE_NAME = 'monprofai-v22';
+var CACHE_NAME = 'monprofai-v23';
 var BASE = '/monprofai-app/';
 var FILES_TO_CACHE = [
   BASE,
